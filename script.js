611cc73af95368a9fafa53844fc0ca84ff12c536
nostalgickd <83178906+nostalgickd@users.noreply.github.com>
2021-10-23 21:23:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,279 @@
+let create= (x)=> document.createElement(x),
+select= (x,y=document)=> y.querySelector(x),
+selectAll= (x,y=document)=> y.querySelectorAll(x);
+
+
+let offline= "/resources/kdradiosorry.mp3",
+srflag= select("#sr"),
+inflag= select("#in"),
+hide= select("#hide"),
+pagetitle= "",
+button= select(".button"),
+container= select("#container"),
+stop= select("#stop"),
+timer= select("#timer");
+
+if(inflag){
+pagetitle= "KD Radio - Suriname";
+inflag.onclick=()=> location= "bollywood.html";				
+}
+
+if(srflag){
+pagetitle= "KD Radio - Bollywood";
+srflag.onclick=()=> location= "/";			
+}
+
+document.body.onselectstart=()=>{
+return false;
+};
+
+
+//Using button to switch genres
+if(list.length > 1){
+main(list[0]);
+button.onclick= change;
+//button.style.display= "flex";
+button.style.visibility= "visible";
+}
+else{
+main(list[0]);
+//button.style.display= "none";
+button.style.visibility= "hidden";	
+}
+
+
+
+/*________________________________Main_Function__________________*/
+function main(list){
+let url= Object.keys(list);
+let name= Object.values(list);
+
+//Create players
+name.forEach((i,x)=>{
+let a= create("span");
+a.className= "span white";
+a.innerHTML= i +`<audio class="audio"></audio>`;
+container.append(a);
+});
+
+//Select players
+audio= selectAll(".audio");
+span= selectAll(".span");
+
+audio.forEach((i,x)=>{
+
+//Clicked
+i.parentNode.onclick=()=>{
+if(i.paused){
+play(i,url[x]);
+timer.innerHTML= `Loading ${i.parentNode.innerText}...`;
+}
+else{
+return false;				
+}
+};
+
+
+
+//Playing
+i.onplay=()=>{
+duration(i);
+i.parentNode.className= "span green";
+document.title= `${i.parentNode.innerText} - KD Radio`;
+navigator.mediaSession.setActionHandler("previoustrack", ()=>skip(x,"prev"));
+navigator.mediaSession.setActionHandler("nexttrack", ()=>skip(x,"next"));
+
+};
+				
+
+//Paused
+i.onpause=()=>{
+i.parentNode.className= "span red";
+document.title=  `${i.parentNode.innerText} [Paused] - KD Radio`;
+timer.innerHTML= `${i.parentNode.innerText} is currently paused`;
+};
+
+});
+
+//Stopping all stations 
+stop.onclick=()=>{
+stopAll();
+document.title= pagetitle;
+timer.innerHTML= "NO RADIO PLAYING";
+};
+
+//Hiding stations
+if(block.length > 0){
+spanDisplay("none");
+hide.onclick= expand;
+//hide.style.display= "inline-flex";
+hide.style.visibility= "visible";
+}
+else{
+//hide.style.display= "none";
+hide.style.visibility= "hidden";
+}
+
+
+//Skip station via notification panel
+function skip(a,b){
+let limit= url.length-1;
+let z= a;
+if(b=="next"){
+if (z==limit) z= -1;
+play(audio[z+1],url[z+1]);
+timer.innerHTML= `Loading ${name[z+1]}...`;
+}
+if(b=="prev"){
+if (z==0) z= limit+1;
+play(audio[z-1],url[z-1]);
+timer.innerHTML= `Loading ${name[z-1]}...`;
+}
+}
+
+
+
+}//End of main function
+
+
+
+
+
+
+
+/*________________________________Helper_Functions__________________*/
+
+//Stop all stations
+function stopAll(){
+audio.forEach(i=>{
+i.innerHTML= ""; i.load();		
+i.parentNode.className= "span white";
+});	
+}
+
+//Play this station--------------------------
+function play(a,b){
+stopAll();
+a.innerHTML=`<source src="${b}"><source src="${offline}">`;
+a.load(); a.play();
+}
+
+
+//Display duration---------------------------
+function duration (station){
+function tijd(x,y="1"){
+if(y==0){
+timer.innerHTML= `Loading ${station.parentNode.innerText}...`;
+}
+else{
+timer.innerHTML= `Listening to ${station.parentNode.innerText} for ${x} seconds`;
+}
+}
+
+station.ontimeupdate= function(){
+
+
+
+let time= new Date(this.currentTime * 1000).
+toISOString().substr(11, 8).split(":");
+let h= +time[0], m= +time[1], s= +time[2];
+
+if(this.currentTime>0) this.parentNode.className= "span playing";
+
+if(!this.paused){
+if(h==0){
+if(m==0) s==0 ? tijd(s,0) : tijd(s);
+if(m==1) tijd(`1 minute & ${s}`);
+if(m >1) tijd(`${m} minutes & ${s}`);
+}
+
+if(h==1){
+if(m==0) tijd(`1 hour & ${s}`);
+if(m==1) tijd(`1 hour, 1 minute & ${s}`);
+if(m >0) tijd(`1 hour, ${m} minutes & ${s}`);
+}
+
+if(h >1){
+if(m==0) tijd(`${h} hours & ${s}`);
+if(m==1) tijd(`${h} hours, 1 minute & ${s}`);
+if(m >0) tijd(`${h} hours, ${m} minutes & ${s}`);
+}
+
+if(this.currentSrc.includes("kdradiosorry")){
+this.parentNode.className= "span red";
+document.title=  `${this.parentNode.innerText} [offline] - KD Radio`;
+timer.innerHTML= `${this.parentNode.innerText} is currently offline`;
+}
+}
+
+controls(document.title.replace(/ ?- ?KD Radio/g,""));
+
+};
+}
+
+//Hiding stations--------------------
+function spanDisplay(a){
+block.forEach(x=> {
+span[x].style.display= a;
+});
+}
+
+//Initiating hide stations-------------
+function expand(){
+hide.classList.toggle("clicked");
+if(hide.classList.contains("clicked"))	{
+hide.innerHTML= "FEW";
+spanDisplay("flex");
+}
+else{
+hide.innerHTML= "All";
+spanDisplay("none");				
+}	
+}
+
+//Check genre-------------
+function taal(a,b,c){
+if(a){
+block= b;
+button.innerHTML= c;		
+}		
+}
+
+//Switch genre-------------
+function change(){
+stopAll();
+document.title= pagetitle;
+timer.innerHTML= "NO RADIO PLAYING";
+span.forEach(i=>i.remove());
+hide.classList.remove("clicked");
+hide.innerHTML= "ALL";
+if(sr==list.length) sr=0;
+//taal((sr==0),[0,4,5,6,8,9],"Sarnamie");
+taal((sr==0),[],"Sarnamie");
+taal((sr==1),[],"General");
+taal((sr==2),[],"Javanese");
+taal((sr==3),[],"Sranang");
+taal((sr==4),[],"Christian");
+taal((sr==5),[],"Other");
+main(list[sr]);
+sr++;
+}
+
+
+//Media controls-----------------
+function controls(a){
+if ('mediaSession' in navigator) {
+navigator.mediaSession.metadata = new MediaMetadata({
+title: a,
+artist: "KD Radio @ " + location.hostname,
+artwork: [
+{ src: '/resources/artwork/background-96x96.png',   sizes: '96x96',   type: 'image/png' },
+{ src: '/resources/artwork/background-128x128.png', sizes: '128x128', type: 'image/png' },
+{ src: '/resources/artwork/background-192x192.png', sizes: '192x192', type: 'image/png' },
+{ src: '/resources/artwork/background-256x256.png', sizes: '256x256', type: 'image/png' },
+{ src: '/resources/artwork/background-384x384.png', sizes: '384x384', type: 'image/png' },
+{ src: '/resources/artwork/background-512x512.png', sizes: '512x512', type: 'image/png' },
+]
+});
+}
+}
---
+++ suriname.js
@@ -0,0 +1,82 @@
+let sarnamie= {
+"http://168.195.218.193:8000/radio1-1" : "Rapar",
+"http://tbn.kloud.xyz:8050/radio.mp3" : "Trishul",
+"http://108.59.9.147:8136/;" : "Rasonic",
+"http://sgm.kloud.xyz:8030/radio.mp3" : "Sangeetmala ",
+"http://nickerie.webhop.net:8000/;?type=http&nocache=5286" : "Ishara",
+//"http://eksitara.kloud.xyz:8120/radio.mp3" : "Ek Sitara",
+"http://79.143.187.96:8053/live" : "Suvidha",
+"http://shruti.kloud.xyz:8000/radio.mp3" : "Shruti",
+"https://amorfm_192.streampartner.nl/stream" : "Amor",
+"http://94.23.148.11:8058/;" : "Vahon",
+"http://stream2.ujala.nl/stream/2/listen.mp3" : "Ujala",
+"http://stream.sunrisefm.nl:9600/stream" : "Sunrise",
+"http://stream2.iqhosted.nl:8000/stream/1/" : "Sangam",
+"https://rs.suricloud.com/radio/8130/radio.mp3" : "Sargam",
+"https://hidradio.live-streams.nl/live?type=.mp3/;stream.mp3" : "HiD Radio"
+};
+
+let general= {
+"http://bombelman.com:5472/;?d=" : "ABC",
+"http://s6.voscast.com:8150/;stream.mp3" : "Apintie",
+"http://streams.radio.co/s28bc986cd/listen" : "Radio 10",
+"http://168.195.218.193:8000/radio1-3" : "RP Hot One",
+"http://168.195.218.193:8000/radio2-2" : "Beat FM",
+"http://sccn104.serverroom.us:5600/;stream.mp3" : "SCCN Smooth",
+"http://pipel.kloud.xyz:8010/radio.mp3" : "Sky Pipel",
+"http://radio9.kloud.xyz:8060/radio.mp3" : "Radio 9",
+"https://surilite1.radioca.st/stream" : "Suri Lite",
+//"http://162.244.80.245:8006/;" : "FM Gold",
+"https://canopus.dribbcast.com/stream/8052/stream/1/" : "Color Radio",
+//"https://solid41.streamupsolutions.com/proxy/yjdqldwt?mp=/stream" : "Da Beat",
+//"http://streaming.shoutcast.com/we-radio?" : "WE Radio",
+"https://caster05.streampakket.com/proxy/9338/stream/1/" : "Tamara",
+"http://bombelman.com:3062/;" : "NIO",
+//"http://127.0.0.1:26671/listen.pls" : "Radio One",
+"http://192.111.140.6:8117/stream" : "Fawaka",
+//"http://listen.shoutcast.com/k1009" : "Kankantri",
+"https://sonic.magicdragon.nl/8024/stream" : "Pokoe FM",
+"https://icecast.omroep.nl/3fm-bb-mp3" : "Radio FM 103",
+"http://cc3b.beheerstream.com:8192/;stream.mp3" : "Cariba FM"
+};
+
+let javanese= {
+"http://162.244.80.245:8012/;" : "Garuda",
+//"http://streaming.radionomy.com/LaguJawa" : "Lagu Jawa",
+"http://94.23.148.11:8214/stream/1/" : "Sirian Jawa",
+"https://s5.radio.co/s1735f3c1d/listen":"Surja Vibes"
+};
+
+let sranang= {
+"http://168.195.218.193:8000/radio2-0" : "SRS",
+"http://centova.pure-isp.eu:8012/stream" : "Konmakandra",
+"https://canopus.dribbcast.com/stream/8052" : "Anjisa",
+"http://162.244.80.245:8020/stream/1/" : "Koyeba",
+"http://sc.sr.net:8000/radio2-1" : "Boskopu",
+//"http://91.196.171.99/radiobrasa" : "Brasa",
+"​https://stream.zenolive.com/6te6g6ubed0uv.aac​​​​​" : "Radio Anda",
+//"http://178.63.94.130:8080/9181316.ogg" : "Positive",
+"http://stanvasteradio.gkstreamen.nl:8120/;stream.mp3" : "Stanvaste",
+"http://46.4.5.234:8080/9173707.ogg" : "Asosye"
+};
+
+let christian= {
+"http://162.244.80.245:8014/;" : "Shalom",
+"http://bombelman.com:3052/random" : "Immanu&euml;l",
+"http://server.jvhost.net:8120/stream" : "Kabar Katresnan",
+//"http://198.7.59.204/stream.mp3?ipport=198.7.59.204_31366" : "Radio 7 Gospel",
+"http://exoticaradio.shoutcaststream.com:8380/stream" : "Radio mArt"
+};
+
+let other= {
+"https://olon.az.icecast.ebsd.ericsson.net/salto_razo" : "Razo",
+//"http://icecast.streamone.net/YoRFHeMSYJ42" : "Caribbean FM",
+"http://bombelman.com:8036/;stream.nsv" : "LPM Portuguese"
+};
+
+
+let list= [sarnamie,general,javanese,sranang,christian,other];
+
+
+let sr= 1, block=[];
+//block= [0,4,5,6,8,9];
