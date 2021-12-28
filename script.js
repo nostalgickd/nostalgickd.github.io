25998207a7a54a387748c28bbdf5f7c06f9e52f1
Mysterious <83178906+nostalgickd@users.noreply.github.com>
2021-12-28 13:20:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let message= `Not all stations can be played, because you're viewing this page o
 
 Click "OK" to load now or "Cancel" to keep using https!`;
 
-
-if(location.protocol=="https:"){
-let alert= confirm(message);
-if(alert) location= location.href.replace(location.protocol,"http:")
+let _https= (location.protocol=="https:");
+if(_https){
+let _alert= confirm(message);
+if(_alert) location= location.href.replace(location.protocol,"http:")
 }
 
 
@@ -14,7 +14,9 @@ let create= (x)=> document.createElement(x),
 select= (x,y=document)=> y.querySelector(x),
 selectAll= (x,y=document)=> y.querySelectorAll(x);
 
-let offline= "/resources/kdradiosorry.mp3",
+let offline= httpz ? 
+"/resources/oopshttps.mp3" :
+"/resources/kdradiosorry.mp3" ;
 srflag= select("#sr"),
 inflag= select("#in"),
 hide= select("#hide"),
@@ -36,6 +38,9 @@ right.innerHTML= `<svg class="dir" viewBox="0 0 24 24"><g>
 <path d="M22 11l-7-9v5C3.047 7 1.668 16.678 2 22c.502-2.685.735-7 13-7v5l7-9z"/>
 </g></svg>`;
 }
+
+genre.innerHTML= liststring.split(",")[0];
+
 
 
 document.body.onselectstart=()=> false;
@@ -110,9 +115,9 @@ stationInfo(i);
 //Display current station info-----
 function stationInfo(x){
 audio.ontimeupdate=()=>{
-
 if(audio.currentTime>1){
-if(audio.currentSrc.includes("kdradiosorry")){
+let isoffline= new URL(offline, location);
+if(audio.currentSrc==isoffline){
   x.className= "span offline";
   document.title= x.innerHTML + " [offline] - KD Radio";
   timer.innerHTML= x.innerHTML + " is offline";
@@ -212,14 +217,12 @@ block.forEach(i=> span[i].style.display= hidden ? "none" : "flex");
 hidden= !hidden;
 }
 
+
 //Check genre--------------
-function checkGenre(x){
-if(x==0) genre.innerHTML= "sarnamie";
-if(x==1) genre.innerHTML= "general";
-if(x==2) genre.innerHTML= "sranang";
-if(x==3) genre.innerHTML= "christian";
-if(x==4) genre.innerHTML= "javanese";
-if(x==5) genre.innerHTML= "other";
+function checkGenre(index){
+liststring.split(",").forEach((i,x)=>{
+if(index==x) genre.innerHTML= i;
+});
 }
 
 
@@ -227,18 +230,19 @@ if(x==5) genre.innerHTML= "other";
 
 //-----Change genres-----------
 let current= 0;
+let maxstations= list.length;
 function change(direction){
 //audio.pause();
 container.innerHTML= "";
 
 if(direction=="right"){
-if(current==5) current= -1;
+if(current==maxstations-1) current= -1;
 current++;
 main(list[current]);
 checkGenre(current);
 }
 else if(direction=="left"){
-if(current==0) current= 6;
+if(current==0) current= maxstations;
 current--;
 main(list[current]);
 checkGenre(current);
@@ -262,3 +266,26 @@ let meta2= create("meta");
 meta2.name= "keywords";
 meta2.content= keywords.join(", ");
 meta1.after(meta2);
+
+
+//INFO LINK
+let about= create("a");
+document.body.append(about);
+about.className= "info";
+about.href= "about.html";
+about.target= "_self";
+about.innerHTML= `
+<svg class= "info" viewBox="0 0 63 64">
+<path d="M55.826.723H7.916A7.29 7.29 0 0 0 .625 8.014v47.91a7.29 7.29 0 
+0 0 7.291 7.291h47.91a7.29 7.29 0 0 0 7.291-7.291V8.014A7.29 7.29 0 0 0 
+55.826.723zm-24.25 53.874c-12.508 0-22.648-10.139-22.648-22.649c0-12.509 
+10.139-22.649 22.648-22.649c12.508 0 22.648 10.14 22.648 22.649c0 
+12.508-10.14 22.649-22.648 22.649z"/>
+<path d="M31.576 14.362c-9.713 0-17.587 7.874-17.587 17.586c0 9.713 7.873 
+17.588 17.587 17.588c9.713 0 17.587-7.875 17.587-17.588c0-9.713-7.874-17.586-17.587-17.586zm2.673 
+32.722h-5.181v-5.318h5.181v5.318zm5.182-16.554c-2.182 2.916-5.182 
+1.745-5.182 6.408v3.055h-5.181V34.81c0-2.317.927-3.98 2.891-4.963c2.774-1.387 
+5.208-4.008 3.026-6.708c-.933-1.155-2.1-1.281-3.463-1.281c-2.29 0-4.582 1.254-4.582 
+4.827c0 1.201-.01 1.81-.01 1.81l-5.035-.011c-.818-6.517 3.435-9.547 4.636-10.363c1.445-.982 
+6.628-2.782 11.208.846c4.004 3.169 4.642 7.618 1.692 11.563z"/>
+</svg>`;
