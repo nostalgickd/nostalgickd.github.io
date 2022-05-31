3e6be2df81438c14fee0c2d10f95e56908e1708a
Mysterious <83178906+nostalgickd@users.noreply.github.com>
2022-05-31 20:14:38 -0300

    .

--- bollywood.js
+++ bollywood.js
@@ -10,18 +10,18 @@ let bollywood= {
 //"https://eu47-sonic.instainternet.com/8068/stream":"Himal Filmy",
 //"https://indifun.net:7000/;":"Indifun",
 "http://www.indifun.net:7000/;":"Indifun",
-"https://securestreams8.autopo.st:3003/1":"90s Once Again",
 //"http://peridot.streamguys.com:7150/Mirchi":"Mirchi FM",
 "https://stream.zeno.fm/vmp0tkewzv8uv":"Caprice",
-"https://stream.zeno.fm/yg1qhsbaqv5tv":"Namkin",
 //"https://prclive1.listenon.in/":"Radio City [Talk]",
 "https://funasia.streamguys1.com/live9":"Big Melodies",
+"https://funasia.streamguys1.com/live4":"Big FM 106.2",
 "https://tunein-icecast.mediaworks.nz/humm_128kbps":"Humm FM",
 //"https://cp12.serverse.com/proxy/hummfm?mp=/live":"Humm FM",
 "https://stream.zeno.fm/2vhb00mhky8uv":"Singham",
 //"https://stream.zeno.fm/d22wrdbst5quv.mp3":"Radio Wow",
 "https://stream.zeno.fm/6rth3ywheg8uv":"BollyHits",
 //"http://hoth.alonhosting.com:1080/;stream.mp3":"BollyHits",
+"http://ca2.radioboss.fm/proxy/guyanac1?mp=/stream":"Guyana Chunes",
 "https://stream.zeno.fm/dhytbz6zfchvv":"Nitnut",
 "https://stream.zeno.fm/a69txeenvzzuv":"SRK Online",
 "https://stream.bongonet.net/proxy/rhi?mp=/stream":"RHI - Covers",
@@ -37,6 +37,10 @@ let bollywood= {
 "https://stream.zeno.fm/y0ce4dbtfa0uv" : "Indias Movies"
 };
 
+let bollywood2= {
+"https://stream.zeno.fm/yg1qhsbaqv5tv":"Namkin",
+"https://securestreams8.autopo.st:3003/1":"90s Once Again",
+};
 
 //--------
 
@@ -61,10 +65,14 @@ liststring= liststring.split(",");
 
 //PROXY OVER HTTPS
 if(location.protocol=="https:"){
-let bollywood1={};
-let list1= [bollywood1];
-list.forEach((i,x)=> addProxy(list[x],list1[x]));
-list= list1;
+let proxiedList= [];
+list.forEach(i=>{
+let clonedObject= {};
+proxiedList.push(clonedObject);
+});
+
+list.forEach((i,x)=> addProxy(list[x],proxiedList[x]));
+list= proxiedList;
 }
 
 
@@ -72,7 +80,7 @@ list= list1;
 let block= "[2,4,5,8,10,12,18,19]";
 let blocklist= localStorage["_block"]|| block;
 block= JSON.parse(blocklist);
-
+block= [];
 
 
 //LOCATION HASH
