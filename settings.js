62a5b90341a494d10c35d0c5d10a2fa8dbb1b99e
Mysterious <83178906+nostalgickd@users.noreply.github.com>
2022-06-13 20:49:18 -0300

    Updated

---
+++ settings.js
@@ -0,0 +1,137 @@
+
+let overlay= create("div");
+overlay.id= "overlay"; overlay.className= "hidden";
+document.body.append(overlay);
+overlay.innerHTML=`
+<div class="normalflex modal">
+	
+	<div class="centerflex"><b>select stations to hide</b></div>
+	<div class="normalflex">
+		<select class="select" multiple></select>
+		<button class="save">SAVE</button>
+	</div>
+	
+	<div class="centerflex msg">
+		<i><span class="nr">0</span>&nbsp; stations hidden!</i>
+	</div>
+	
+	<div class="centerflex"><b>Close tab after ?? minutes<b></div>
+	<div class="normalflex">
+		<input class="input" type="number">
+		<button class="sleep">SLEEP</button>
+	</div>
+	
+	<div class="centerflex close">×</div>
+</div>`;
+
+
+let stationList= select(".modal .select");
+let save= select(".modal .save");
+let msg= select(".modal .nr");
+
+let lsblock=()=> genre.innerHTML + "block";
+let selectedStations=()=> [...stationList.selectedOptions].map(i=> i.index);
+
+
+
+function manageHidden(){
+stationList.value= stationList.innerHTML= "";
+let optgroup= create("optgroup");
+optgroup.label= "--- Select which stations to hide ---";
+stationList.append(optgroup);
+spans.forEach(i=>{
+let option= create("option");
+option.innerHTML= i.textContent;
+stationList.append(option);
+});
+
+let blocklist= localStorage[lsblock()];
+blocklist= blocklist ? JSON.parse(blocklist) : ibl[genre.innerHTML];
+
+let options= selectAll(".modal option");
+blocklist.forEach(i=>{
+options[i].selected= true;
+spans[i].classList.add("hidden");
+});
+
+hide.innerHTML= "ALL";
+}
+
+
+
+
+
+
+
+hide.onclick=()=>{
+let hidden= select(".span.hidden");
+if(hidden){
+hide.innerHTML= "FEW";
+spans.forEach(i=>i.classList.remove("hidden"));
+}
+else{
+hide.innerHTML= "ALL"
+selectedStations().forEach(i=> spans[i].classList.add("hidden"));
+}
+};
+
+
+
+	
+//hide.onclick=()=> manageHidden(false);
+select(".about").onclick=()=> window.open("about");
+select(".modal .close").onclick=()=> overlay.className= "hidden";
+stationList.oninput=()=> save.innerHTML= "SAVE";
+
+select(".settings").onclick=()=>{
+overlay.className= "unhidden";
+save.innerHTML= "SAVE";
+msg.innerHTML= selectedStations().length;
+};
+
+save.onclick=()=>{
+save.innerHTML= "SAVED";
+hide.innerHTML= "ALL";
+msg.innerHTML= selectedStations().length;
+spans.forEach(i=> i.classList.remove("hidden")); 
+selectedStations().forEach(i=> spans[i].classList.add("hidden"));
+localStorage[lsblock()]= JSON.stringify(selectedStations());
+};
+
+manageHidden(true);
+
+//Sleeptimer-------------------------------------------
+select(".sleep").onclick=()=>{
+let mins= select(".input").value;
+let html= select("html");
+if(mins){
+let tab= window.open(location.href);
+let time= mins*60*1000;
+let left= time;
+writeIt(time,html,left);
+
+setTimeout(()=>{
+tab.close();
+},time);
+}
+else alert("INVALID TIME");
+};
+
+
+
+function writeIt(x,y,z){
+if(x>1000){
+y.innerHTML= `>>Tab opened in a new tab!<br>
+>>It will close in ${formatTime(x)}`;
+x= x-1000;
+setTimeout(()=>{
+writeIt(x,y,z);
+document.title= "Sleep Timer - KD Radio";
+},1000);
+}
+else{
+y.innerHTML= `>>Tab opened in a new tab!<br>
+>>Closed after ${formatTime(z)}`;
+document.title= "Sleep Timer - KD Radio";
+}
+}
---
+++ suriname.js
@@ -0,0 +1,130 @@
+let sarnamie= {
+"https://stream.zeno.fm/bu26ykfcxnhvv" : "AVS Classic",
+"https://stream.zeno.fm/p218h6ccxnhvv" : "AVS Radio",
+"http://stream.akaashfm.com:8000/live" : "Akaash FM",
+"https://amorfm_192.streampartner.nl/stream" : "Amor",
+"https://stream.zeno.fm/xssh6qrf1wzuv" : "Assalaam",
+"https://stream.zeno.fm/putkar0p9k0uv" : "Bollywood Sur.",
+"http://www.dhantalradio.ca:8000/stream1" : "Dhantal",
+"https://rs.suricloud.com/radio/8120/radio.mp3" : "Ek Sitara",
+"https://hidradio.live-streams.nl/live?type=.mp3/;stream.mp3" : "HiD Radio",
+"https://stream.hostinggold.eu:1185/stream" : "Hulchul",
+"https://stream.zeno.fm/nqm3a44vzv8uv" : "Ishara",
+"https://stream.zeno.fm/ctmsytc6em0uv" : "Joyce The Voice",
+"http://mscp3.live-streams.nl:8050/stream" : "Pehchaan",
+"https://stream.zeno.fm/kzsmp5qs9k0uv" : "Radio Sarnami",
+"https://audio-edge-es6pf.mia.g.radiomast.io/d7e8093b-6a7e-4442-a096-37f259cb7092" : "Rapar",
+"https://stream.zeno.fm/6etys88fup8uv" : "Rasonic",
+"https://solid48.streamupsolutions.com/proxy/radiosbs/radiosbs" : "SBS Radio",
+"http://stream2.iqhosted.nl:8000/stream/1/" : "Sangam",
+"https://rs.suricloud.com/radio/8030/radio.mp3" : "Sangeetmala",
+"https://rs.suricloud.com/radio/8130/radio.mp3" : "Sargam",
+"https://rs.suricloud.com/radio/8000/radio.mp3" : "Shruti",
+"https://server-25.stream-server.nl:8348/stream" : "Sitara FM",
+"https://stream.zeno.fm/d9wgghcwzv8uv" : "Srinagar",
+"https://stream.sfmstreaming.nl/sunrisefm" : "Sunrise FM",
+"https://my.ssl-stream.com/Suvidha_Radio" : "Suvidha",
+"https://rs.suricloud.com/radio/8050/radio.mp3" : "Trishul",
+"https://server4.ujala.nl/stream/2/listen.mp3" : "Ujala",
+"https://onlineradio.websoftitnepal.com/8030/stream/1/.mp4a.40.5" : "Vahon"
+};
+
+
+let general= {
+ "https://stream.zeno.fm/7qt8yd08qchvv": "ABC",
+ "https://stream.zeno.fm/zh2rs490x68uv": "Apintie",
+ "https://168.195.218.193:8000/radio2-2": "Beat FM",
+ "http://cc3b.beheerstream.com:8192/;stream.mp3": "Cariba FM",
+ "https://canopus.dribbcast.com/stream/8052/stream/1/": "Color Radio",
+ "https://radiostream.limfmsu.com/radio": "LIM FM",
+ "https://stream.zeno.fm/5tv3n9cw008uv": "Lime",
+ "https://canopus.dribbcast.com/stream/8010": "NIO FM",
+ "https://larry.torontocast.com:1795/stream": "Neutral",
+ "https://168.195.218.193:8000/radio1-3": "RP Hot One",
+ "https://streams.radio.co/s28bc986cd/listen": "Radio 10",
+ "https://stream.zeno.fm/xsdmzxyhc18uv": "Radio 804",
+ "https://rs.suricloud.com/radio/8060/radio.mp3": "Radio 9",
+ "https://icecast.omroep.nl/3fm-bb-mp3": "Radio FM 103",
+ "http://sccn104.serverroom.us:5600/;stream.mp3": "SCCN Smooth",
+ "https://surilive.com:8060/;": "SRS",
+ "https://rs.suricloud.com/radio/8010/radio.mp3": "Sky - Pipel",
+ "http://stanvasteradio.gkstreamen.nl:8120/;": "Stanvaste",
+ "https://stream.rcast.net/67505": "Suri Lite",
+ "https://caster05.streampakket.com/proxy/9338/stream/1/": "Tamara"
+};
+
+
+
+
+
+
+let javanese= {
+ "https://betelgeuse.dribbcast.com/proxy/abhost?mp=/stream": "Bangsa Jawa",
+ "https://stream.zeno.fm/w6hh0rqqwy8uv": "DjRegi",
+ "https://worldstreaminglive.com/proxy/apzrueeh/stream": "Garuda",
+ "http://94.23.148.11:8214/stream/1/": "Siaran Jawa",
+ "https://s5.radio.co/s1735f3c1d/listen": "Surja Vibes",
+ "https://stream.zeno.fm/u3abxhenhf9uv": "Tamanredjo Talents"
+};
+
+
+
+let sranang= {
+ "https://canopus.dribbcast.com/stream/8052": "Anjisa",
+ "https://stream.zeno.fm/q4wkxqmdqchvv": "Asosye",
+ "https://stream.zeno.fm/rnf3xu9tgs8uv": "Boeskondee Media",
+ "https://stream.zeno.fm/abb1cne92hhvv": "DC SuriRadio",
+ "https://stream.zeno.fm/db9pak567ehvv": "DJBadJho",
+ "https://stream.zeno.fm/p0d98x5phs8uv": "DoubleB 2",
+ "https://stream.zeno.fm/zduxspz4kv8uv": "Fan Da Libi",
+ "https://stream.zeno.fm/1sqwfhqwra0uv": "Fandaaki Paanzu",
+ "https://patmos.cdnstream.com/proxy/rifawaka/?mp=/stream": "Fawaka",
+ "https://stream.zeno.fm/ysd1dvrtcv8uv": "Ketebuna",
+ "https://mediaserv30.live-streams.nl:18020/stream": "Konmakandra",
+ "https://worldstreaminglive.com/proxy/koyeba/stream": "Koyeba FM",
+ "https://clare.shoutca.st/radio/8070/radio.mp3": "MS Radio",
+ "https://stream.zeno.fm/unz726g6kfhvv": "Maipa",
+ "https://stream.zeno.fm/gg7afgcuhkhvv": "Makandi",
+ "https://stream.zeno.fm/y3k2cb2mpchvv": "Ollies Radio",
+ "https://sonic.magicdragon.nl:7011/stream": "Pokoe FM",
+ "https://stream.zenolive.com/6te6g6ubed0uv.aac": "Radio Anda",
+ "https://stream.zeno.fm/kyve9mgds18uv": "Radio Totness",
+ "https://stream.zeno.fm/ym53qqc53hhvv": "Sam Taki"
+};
+
+
+
+let christian= {
+"https://stream.zeno.fm/kadt188ugy8uv" : "Afonsoewa",
+"https://stream.zeno.fm/6nt6k3e2x68uv" : "Immanu&euml;l",
+"http://server.jvhost.net:8010/live" : "Kabar Katresnan",
+"https://stream.radiomaria.nl/mp3" : "Radio Maria",
+"https://cc5.beheerstream.com/proxy/exoticaradio?mp=/stream" : "Radio mArt",
+"https://worldstreaminglive.com/proxy/radioshalom/stream" : "Shalom",
+//https://198.7.59.204/stream.mp3?ipport=198.7.59.204_31366" : "Radio 7 Gospel",
+};
+
+
+
+let other= {
+"https://d1x0scdnoh33b7.cloudfront.net/olonradio/7fb21f1b/master.m3u8" : "Caribbean FM",
+"https://surilive.com:8050/stream/1/" : "LPM Portugese",
+"https://stream.zeno.fm/w0hf496vvhhvv" : "Paw Matu Radio",
+"https://d1x0scdnoh33b7.cloudfront.net/olonradio/537b9f27/master.m3u8" : "Razo",
+//"/resources/placeholder.aac" : "Placeholder"
+};
+
+
+//Main list
+let list= [sarnamie,general,sranang,christian,javanese,other];
+
+//BlockList
+let ibl= {
+"sarnamie" : [0,1,4,5,9,11,12,13],
+"general" : [],
+"sranang" : [],
+"christian" : [],
+"javanese" : [],
+"other" : []
+};
+
